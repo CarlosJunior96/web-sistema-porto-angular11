6996067040f4334dc1644734f10fe43aacb726d2
Carlos Junior Silva <cjrcordeiro@gmail.com>
2021-02-22 17:02:25 -0300

    Criando componente formulario do consumo agua

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
+/** IMPORTACAO PARA USO DO FORMS DO ANGULAR E SER POSSÍVEL TRABALHAR DE FORMA DINAMICA **/
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+/** IMPORTANDO CLASSES RESPONSAVEIS PARA FAZER AS ROTAS **/
+import { RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ConsumoAguaComponent } from './consumo-agua/consumo-agua.component';
+import { ConsumoOleoComponent } from './consumo-oleo/consumo-oleo.component';
+import { ConsumoCombustivelComponent } from './consumo-combustivel/consumo-combustivel.component';
+import { ResiduoOrganicoMarComponent } from './residuo-organico-mar/residuo-organico-mar.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
+
+/** VETOR QUE RECEBE AS ROTAS QUE O SISTEMA TERÁ **/
+const rotasApp: Routes = [
+  {path: "agua-navio", component: ConsumoAguaComponent},
+  {path: "combustivel-navio", component: ConsumoCombustivelComponent},
+  {path: "oleo-lubrificante-navio", component: ConsumoOleoComponent},
+  {path: "residuo-organico-navio", component: ResiduoOrganicoMarComponent},
+  {path: "", component: InicioComponent}, /** se tiver apenas o url geral será redirecionado para o inicio **/
+  {path: "**", component: PaginaNaoEncontradaComponent} /** se não existir nenhum dos endereços informados vai pra essa pagina**/
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConsumoAguaComponent
+    ConsumoAguaComponent,
+    ConsumoOleoComponent,
+    ConsumoCombustivelComponent,
+    ResiduoOrganicoMarComponent,
+    InicioComponent,
+    PaginaNaoEncontradaComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(rotasApp, {enableTracing: true}) /** importando o modulo de rotas, passando o vetor de rotas como parametro
+     e ativando o registro de eventos **/
   ],
   providers: [],
   bootstrap: [AppComponent]

0a69c35cb098aa67907a459afd3367f9a8566c84
Carlos Junior Silva <cjrcordeiro@gmail.com>
2021-02-23 12:22:33 -0300

    testado get e post do agua navio e criar navio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { ConsumoCombustivelComponent } from './consumo-combustivel/consumo-combu
 import { ResiduoOrganicoMarComponent } from './residuo-organico-mar/residuo-organico-mar.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
+import {HttpClientModule} from '@angular/common/http';
+import { NavioComponent } from './navio/navio.component';
 
 /** VETOR QUE RECEBE AS ROTAS QUE O SISTEMA TERÁ **/
 const rotasApp: Routes = [
@@ -21,6 +23,7 @@ const rotasApp: Routes = [
   {path: "combustivel-navio", component: ConsumoCombustivelComponent},
   {path: "oleo-lubrificante-navio", component: ConsumoOleoComponent},
   {path: "residuo-organico-navio", component: ResiduoOrganicoMarComponent},
+  {path: "navio", component: NavioComponent},
   {path: "", component: InicioComponent}, /** se tiver apenas o url geral será redirecionado para o inicio **/
   {path: "**", component: PaginaNaoEncontradaComponent} /** se não existir nenhum dos endereços informados vai pra essa pagina**/
 ]
@@ -33,11 +36,13 @@ const rotasApp: Routes = [
     ConsumoCombustivelComponent,
     ResiduoOrganicoMarComponent,
     InicioComponent,
-    PaginaNaoEncontradaComponent
+    PaginaNaoEncontradaComponent,
+    NavioComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot(rotasApp, {enableTracing: true}) /** importando o modulo de rotas, passando o vetor de rotas como parametro
      e ativando o registro de eventos **/
